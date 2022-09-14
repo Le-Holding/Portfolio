369fca4bddce541995466186b974d4cabe85a73d
Le-Holding <68299863+Le-Holding@users.noreply.github.com>
2022-09-14 10:35:47 +0200

    change ageCalculator to textnode.insertData by id in function

--- calcBirthday.js
+++ calcBirthday.js
@@ -1,23 +1,21 @@
-let x = ageCalculator("1986-11-01"); 
-const span = document.getElementById("MathiasAge");
-const textnode = span.nextSibling;
-textnode.insertData(1, `${x} `)
+ageCalculator("1986-11-01", "MathiasAge");
 
-
-function ageCalculator(birth){
+function ageCalculator(birth, elementId) {
     var dob = new Date(birth);
     //calculate month difference from current date in time
     var month_diff = Date.now() - dob.getTime();
-    
+
     //convert the calculated difference in date format
-    var age_dt = new Date(month_diff); 
-    
-    //extract year from date    
+    var age_dt = new Date(month_diff);
+
+    //extract year from date
     var year = age_dt.getUTCFullYear();
-    
+
     //now calculate the age of the user
     var age = Math.abs(year - 1970);
-    
+
     //display the calculated age
-    return age;
+    const span = document.getElementById(elementId);
+    const textnode = span.nextSibling;
+    textnode.insertData(1, `${age} `)
 }

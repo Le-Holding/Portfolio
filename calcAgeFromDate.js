408db193225b0ef3fc692df0eddcd42ad87971d4
Le-Holding <68299863+Le-Holding@users.noreply.github.com>
2022-09-14 13:07:56 +0200

    change filename calcBirthday to calcAgeFromDate.js

---
+++ calcAgeFromDate.js
@@ -0,0 +1,24 @@
+ageCalculator("1986-11-01", "MathiasAge");
+ageCalculator("2017-04-03", "girlOneAge");
+ageCalculator("2019-08-27", "boyOneAge");
+ageCalculator("2015-05-30", "marriedAge");
+
+function ageCalculator(birth, elementId) {
+    var dob = new Date(birth);
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff);
+
+    //extract year from date
+    var year = age_dt.getUTCFullYear();
+
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+
+    //display the calculated age
+    const span = document.getElementById(elementId);
+    const textnode = span.nextSibling;
+    textnode.insertData(0, age)
+}
